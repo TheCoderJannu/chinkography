53ab352a920f3d9f34f3cb85cdbd88e20edb295c
janhvi suthar <janhvisuthar1210@gmail.com>
2026-07-24 22:31:39 +0530

    Update website again and again and again and again

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -110,7 +110,7 @@ export default function Gallery() {
   { id: 22, 
     type: 'video', 
     url: 'https://www.instagram.com/reel/DS4MvxZEoVr/?igsh=MTI4aDNzNXIzeDA3Mg==', 
-    thumbnail: 'imgs/23rdofit.PNG', 
+    thumbnail: '/imgs/pink hoodie.png', 
     title: 'Fashion Brand@DeeClothing', 
     category: 'commercial-films', 
   },
